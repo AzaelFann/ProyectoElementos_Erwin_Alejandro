b210f7fed5d7564d3c2b6079421c9c2dbba2d6f0
Alejandro <Alejandro@10.250.0.59>
2016-11-22 21:48:26 -0600

    XD

--- src/main/java/org/unitec/BebidasAlcoholicas.java
+++ src/main/java/org/unitec/BebidasAlcoholicas.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
  */
 public class BebidasAlcoholicas implements TipoBebidas {
     
-    String nombre;
+    String     nombre;
     
    public String obtenerRecomendacion(){
         
