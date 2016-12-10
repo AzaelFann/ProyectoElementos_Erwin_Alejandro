1ac06956e8c817b0c3ce71242ebc828cabf741da
Alejandro <Alejandro@10.250.0.59>
2016-12-09 19:32:59 -0600

    modificado

--- src/main/java/org/unitec/ElementosApplication.java
+++ src/main/java/org/unitec/ElementosApplication.java
@@ -1,12 +1,16 @@
 package org.unitec;
 
+import javax.swing.JFrame;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class ElementosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ElementosApplication.class, args);
+          
+          
 	}
 }
